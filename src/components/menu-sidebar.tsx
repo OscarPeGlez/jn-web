505b884621d53182cea9139b05bab115b6be044e
unknown <edmeixueiro@gmail.com>
2020-06-02 23:36:33 -0500

    Cambios

--- src/components/menu-sidebar.tsx
+++ src/components/menu-sidebar.tsx
@@ -1,25 +1,33 @@
-import React, { FC } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-const Menu: FC = () => {
+import Furniture from '../screens/categories/furnitures';
+import Kitchen from '../screens/categories/kitchen';
+import Others from '../screens/categories/others';
+import TableLine from '../screens/categories/tableLinen';
+import Rents from '../screens/rentas';
+
+const Menu = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Link uno </h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1>Link dos </h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1>Link tres </h1>
-        </Col>
-      </Row>
-    </Container>
+    <BrowserRouter>
+      <Navegation />
+      <Route path="/cocina" exact component={Kitchen} />
+      <Route path="/mobiliario" component={Furniture} />
+      <Route path="/otros" component={Others} />
+      <Route path="/manteleria" component={TableLine} />
+      <Route path="/rentas" component={Rents} />
+    </BrowserRouter>
   );
 };
+
+const Navegation = () => (
+  <nav>
+    <NavLink to="/cocina" exact activeClassName="active">Cocina</NavLink>
+    <NavLink to="/mobiliario" activeClassName="active">Mobiliario</NavLink>
+    <NavLink to="/otros" activeClassName="active">Otros</NavLink>
+    <NavLink to="/manteleria" activeClassName="active">Manteleria</NavLink>
+    <NavLink to="/rentas" activeClassName="active">Rentas</NavLink>
+  </nav>
+);
+
 export default Menu;
--- src/screens/categories/kitchen.tsx
+++ src/screens/categories/kitchen.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
 
+import AddElement from '../../components/modal-addElement';
+
 const KitchenInventory: FC = () => {
-  return <h1>Este es el faking listado de cocina :B</h1>;
+  return (
+    <>
+      <h1>Este es el faking listado de cocina :B</h1>
+      <AddElement />
+    </>
+  );
 };
 
 export default KitchenInventory;
